34358ddd02948150fb008c8511a28f63d89456c5
specnazm <52412989+specnazm@users.noreply.github.com>
2022-06-23 20:44:11 +0200

    refactor: pull images from dockerhub (#16)
    * feat: added github-actions config (#9)
    
    * Workflow (#10)
    
    * feat: added github-actions config
    
    * fix: changed name from main to master
    
    Co-authored-by: permitt <perovicmitar@gmail.com>
    
    * fix: added post service (#11)
    
    * fix: mongodb configured (#12)
    
    * feat: added messaging service (#13)
    
    * fix: depends on condition added (#14)
    
    * fix: pull images from dockerhub (#15)
    
    * fix: depends on condition added
    
    * refactor: pull images from dockerhub
    
    Co-authored-by: permitt <perovicmitar@gmail.com>
    Co-authored-by: alenm1010 <93643067+alenm1010@users.noreply.github.com>

---
+++ data/post-server-import.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+<<COMMENT
+mongoimport is a tool for importing data into mongodb.
+Into database users we add collection users and documents are defined in user_data.json.
+For each user, username = password + '123', e.g.(username: nikola, password: nikola123)
+
+--host param is the name of the mongodb (in our case, mongodb service is called auth-server-db in docker-compose.yaml)
+
+--drop param will remove the whole collection before importing the data
+--mode=upsert can be used instead of --drop. This will just update existing keys.
+COMMENT
+
+mongoimport --host post-server-db --authenticationDatabase admin \
+--db user_post --collection posts \
+--username root --password password \
+--type json --mode=upsert \
+--file data/posts_data.json --jsonArray
